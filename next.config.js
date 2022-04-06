1aef2252b5026170998db00b01087064893ba9e8
Ilkka <ilkka.kuosmanen@lightningchart.com>
2022-04-06 14:52:01 +0000

    Remove console on prod build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental:{
-    outputStandalone: true
-  }
-}
 
-module.exports = nextConfig
+const { PHASE_PRODUCTION_BUILD } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      outputStandalone: true
+    },
+    compiler: {
+      removeConsole: false,
+    },
+  }
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    nextConfig.compiler.removeConsole = true
+  }
+  return nextConfig
+}
